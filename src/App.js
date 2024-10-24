639ed1a4b0d6dfb0cf7eb654aa26d929b618cdb3
yanrodrigues205 <yan.pablo205@gmail.com>
2024-10-24 16:34:46 -0300

    :sparkles: feat: add new routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
-// src/App.js
 import "./global.css";
 import Login from "./pages/Login";
 import TwoFactor from "./pages/TwoFactor";
-import { useRoutes, useLocation } from "react-router-dom"; 
+import { useRoutes, useLocation, Navigate } from "react-router-dom"; 
 import WorkHours from "./pages/WorkHours";
 import Wastes from "./pages/Wastes";
 import CustomNavbar from "./components/Navbar";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import CollectPoints from "./pages/CollectPoints";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound"; // Importando NotFound
+import Points from "./pages/Points";
+import CollectUser from "./pages/CollectUser";
+import Contact from "./pages/Contact";
 
 function AppRoutes() {
   let routes = useRoutes([
-    { path: "/", element: <Login /> },
+    { path: "/", element: <Home /> },
+    { path: "/account", element: <Login /> },
     { path: "/two_factor", element: <TwoFactor /> },
     { path: "/work_hours", element: <WorkHours /> },
     { path: "/wastes", element: <Wastes /> },
     { path: "/collect_points", element: <CollectPoints /> },
+    { path: "/points", element: <Points /> },
+    { path: "/collect_user", element: <CollectUser /> },
+    { path: "/contact", element: <Contact /> },
+    { path: "/404", element: <NotFound /> },  // Rota 404 explícita
+    { path: "*", element: <Navigate to="/404" /> },  // Redireciona rotas inexistentes para 404
   ]);
   return routes;
 }
@@ -23,10 +33,16 @@ function AppRoutes() {
 export default function App() {
   const location = useLocation(); // Para verificar a rota atual
 
+  // Array de rotas onde a Navbar NÃO deve ser exibida
+  const hideNavbarRoutes = ["/", "/account", "/contact", "/two_factor", "/404", "/points", "/collect_user"];
+
+  // Verifica se a rota atual está no array de rotas para esconder a Navbar
+  const shouldHideNavbar = hideNavbarRoutes.includes(location.pathname);
+
   return (
     <div>
-      {/* Exibir Navbar exceto nas rotas "/" e "/two_factor" */}
-      {location.pathname !== "/" && location.pathname !== "/two_factor" && (
+      {/* Exibir Navbar apenas se a rota não estiver no array */}
+      {!shouldHideNavbar && (
         <CustomNavbar />
       )}
       <AppRoutes />
